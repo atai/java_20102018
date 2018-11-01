63836124c471e4aed6d3614e62ecce97c5066403
atai <rystam.atai@gmail.com>
2018-11-01 08:15:10 +0300

    Выполнил задание 4 и переформатировал папку курса.

---
+++ lesson4/main/src/sample/Main.java
@@ -0,0 +1,28 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+/*
+1. Создать окно для клиентской части чата: большое текстовое поле для отображения переписки в центре окна.
+Однострочное текстовое поле для ввода сообщений и кнопка для отсылки сообщений на нижней панели.
+Сообщение должно отсылаться либо по нажатию кнопки на форме, либо по нажатию кнопки Enter.
+При «отсылке» сообщение перекидывается из нижнего поля в центральное.
+ */
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
